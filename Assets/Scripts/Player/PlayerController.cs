21da714f8e5e64c5feb3e061512c4bd04512bbf2
Invisibleglass <122887946+Invisibleglass@users.noreply.github.com>
2023-02-06 18:53:28 -0700

    Lab 3 script working

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -59,19 +59,82 @@ public class PlayerController : MonoBehaviour
             // Update is called once per frame
     void Update()
     {
+        AnimatorClipInfo[] curPlayingClip = anim.GetCurrentAnimatorClipInfo(0);
         float hInput = Input.GetAxisRaw("Horizontal");
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        if (curPlayingClip.Length > 0)
+        {
+            if (Input.GetButtonDown("Fire1") && curPlayingClip[0].clip.name != "Fire")
+            {
+                anim.SetTrigger("fire");
+            }
+            else if (curPlayingClip[0].clip.name == "Fire")
+            {
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+                rb.velocity = moveDirection;
+            }
+
+        }
 
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.velocity = Vector2.zero;
             rb.AddForce(Vector2.up * jumpForce);
         }
-        Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
-        rb.velocity = moveDirection;
+
+        if (!isGrounded && Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("JumpAttack");
+        }
+
 
         anim.SetFloat("hInput", Mathf.Abs(hInput));
         anim.SetBool("isGrounded", isGrounded);
+
+        //check for flipped and create an algorithm to flip your character
+        if (hInput != 0)
+            sr.flipX = (hInput < 0);
+
+        if (isGrounded)
+            rb.gravityScale = 1;
     }
+    public void IncreaseGravity()
+    {
+        rb.gravityScale = 5;
+    }
+
+    // use for powerup and if you want to collide with stuff
+    /*    private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Powerup"))
+            {
+                //do something
+            }
+        }
+        private void OnTriggerExit2D(Collider2D other)
+        {
+
+        }
+        private void OnTriggerStay2D(Collider2D other)
+        {
+
+        }
+        private void OnCollisionEnter2D(Collider2D collison)
+        {
+
+        }
+        private void OnCollisionExit2D(Collider2D collison)
+        {
+
+        }
+        private void OnCollisionStay2D(Collider2D collison)
+        {
+
+        }*/
 }
